24d6abde7fdb4d6b0713b6747482b6e4ee0fa4b3
AndreySavin <AndreySavin4@ukr.net>
2026-08-21 00:00:03 +0200

    completed task 3/3 with Promise and setTimeout

--- js/03-promises.js
+++ js/03-promises.js
@@ -1,8 +1,40 @@
+const refs = {
+  delay: document.querySelector("input[name=delay]"),
+  step: document.querySelector("input[name=step]"),
+  amount: document.querySelector("input[name=amount]"),
+  form: document.querySelector(".form"),
+};
+
+const { delay, step, amount, form } = refs;
+
+form.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+
+  let newDelay = Number(delay.value);
+
+  for (let i = 1; i <= amount.value; i += 1) {
+    createPromise(i, newDelay);
+
+    newDelay += Number(step.value);
+  }
+});
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  })
+    .then(({ position, delay }) => {
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
 }
